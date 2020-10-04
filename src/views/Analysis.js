c0a561a3231a386eb959114c82c34ff9b3af1a27
CassioGenehrF <cassiogenehrfilho@gmail.com>
2020-10-04 16:43:38 -0300

    commit-test

--- src/views/Analysis.js
+++ src/views/Analysis.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import { red } from '@material-ui/core/colors'
+// import { makeStyles } from '@material-ui/core/styles'
+// import { red } from '@material-ui/core/colors'
 import BarChart from './BarChart'
 
 export default function CardView() {
